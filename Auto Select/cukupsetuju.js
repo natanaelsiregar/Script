1df21e1414b52b7acdd8bdb643bfe3e1ef4e973a
Natanael Jansudin Siregar <89391887+natanaelsiregar@users.noreply.github.com>
2022-06-30 17:57:04 +0700

    Update cukupsetuju.js

--- Auto Select/cukupsetuju.js
+++ Auto Select/cukupsetuju.js
@@ -7,3 +7,13 @@ var natanael = 50;
 for(let i = 0; i < natanael; i++){
     $('.radio-inline').eq(2 + (6 * i)).click();
 }
+
+/**
+ * option: CUKUP
+ * author @natgharz
+ */
+
+var natanael = 50;
+for(let i = 0; i < natanael; i++){
+    $('.radio-inline').eq(2 + (4 * i)).click();
+}
